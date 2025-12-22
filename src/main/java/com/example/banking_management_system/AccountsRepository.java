830c930763aa9a8c2d0fc776ea8ad211e815afef
ashishmishra3648 <am8419941@gmail.com>
2025-12-22 18:31:08 +0530

    Add AccountsRepository.java

---
+++ src/main/java/com/example/banking_management_system/AccountsRepository.java
@@ -0,0 +1,8 @@
+package com.example.banking_management_system;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// This interface gives CRUD operations on Accounts table
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+    java.util.List<Accounts> findByEmail(String email);
+}
